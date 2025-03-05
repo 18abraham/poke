9df4d46f1a0a4a6065b2111d5f7f22bb4981370c
abraham romo <utm23090466@utma.edu.mx>
2025-03-05 10:35:31 -0600

    actualizacion

--- app/models/pokemon_favorities.py
+++ app/models/pokemon_favorities.py
@@ -11,5 +11,12 @@ class PokemonFavorites(SuperClase):
         
     def update(self, object_id, data):
             raise NotImplementedError("Los Pokemones No Se Pueden Actualizar")
+    
+    def create(self, data):
+          data["user_id"] = ObjectId(data["user_id"])
+          data["pokemon_id"] = ObjectId(data["pokemon_id"])
+          datum = self.collection.insert_one(data)
+          return str(datum.inserted_id)   
+
         
     
--- app/schemas/pokemon_favorites_schema.py
+++ app/schemas/pokemon_favorites_schema.py
@@ -1,14 +1,6 @@
 from marshmallow import Schema, fields, ValidationError
 
 class PokemonFavoritiesSchema(Schema):
-    user_id = fields.Str(
-        required = True,
-        validate=lambda x: len(x) > 0,
-        error_messages={
-            "required": "El Id del usuario es requerido"
-        } 
-        
-    )
 
     pokemon_id = fields.Str(
         required = True,
